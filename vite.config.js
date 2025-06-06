72c504af9f0cc060761f57f3a3d41b2f2c249289
7rashid <57944162+7rashid@users.noreply.github.com>
2025-06-06 19:05:10 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/solarcheck-webapp/', // WICHTIG! Name deines Repos mit / am Anfang und Ende
+  base: '/solarcheck-webapp/',
   plugins: [react()],
-})
+});
